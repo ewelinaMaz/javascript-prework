93fafccad9411256581e9d984b595d731e0c82f2
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2019-12-04 11:29:52 +0100

    Add computer move logic

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Wygrana!');
+let randomNumber = Math.floor(Math.random()*3+1);
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber==1) {
+  computerMove = 'kamień';
+printMessage('Mój ruch to: ' + computerMove);
+}
+
+else if(randomNumber==2) {
+  computerMove = 'papier';
+printMessage('Mój ruch to: ' + computerMove);
+}
+
+else if(randomNumber==3) {
+  computerMove = 'nożyce';
+printMessage('Mój ruch to: ' + computerMove);
+}
