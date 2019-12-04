1d4d9b349a7c806f24c2da85ae9b500beeb9d27b
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2019-12-04 10:09:51 +0100

    Add first js command

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Wygrana!');
